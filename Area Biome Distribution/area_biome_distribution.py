218ba8d3078eb9c45ba14a7a7622d93d79cf2447
confusingProgram <doandy@sheridancollege.ca>
2026-02-04 10:25:34 -0500

    Started area_biome_distribution, for the purpose of generating biome distribution within a region

--- Area Biome Distribution/area_biome_distribution.py
+++ Area Biome Distribution/area_biome_distribution.py
@@ -5,6 +5,35 @@ import os
 class Area:
     def __init__(self, name):
         self.name = name
-        self.file_format_name = "" # Snake Case
+        self.snake_case_name = "" # Snake Case
         self.biomes = []
+        self.biome_pixels = {}
         self.distribution = {}
+        self.pixel_total = 0
+        self.target_colour_rgb = (80, 47, 121)
+
+        area_name_split=self.name.lower().split(" ")
+        area_name = ""
+        for token in area_name_split:
+            area_name += token + "_"
+        self.snake_case_name = area_name[:len(area_name)-1]
+
+    def count(self, image_path):
+        # Expects image path to be something like 3Paldea_Inlet_Grotto_Map_Cave.png
+        arr = image_path.split("_")
+        biome_arr = arr[len(arr)-1].split(".")
+        biome = biome_arr[0]
+        if biome not in self.biomes:
+            self.biomes.append(biome)
+            bp = ccp(image_path, self.target_colour_rgb)
+            self.biome_pixels[biome] = bp
+            self.pixel_total += bp
+        self.biomes.sort()
+
+    def calculate_distribution(self):
+        for biome in self.biomes:
+            percentage = self.biome_pixels[biome] / self.pixel_total
+            self.distribution[biome] = percentage
+
+    def csv(self):
+        pass
